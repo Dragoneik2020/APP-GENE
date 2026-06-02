e759192e6be9dda341163b605ad75fd0d7121d54
Dragoneik <dragoneik2010@gmail.com>
2026-06-01 23:48:57 -0400

    Add frontend service and update API URL

--- FitGenius/src/services/api.ts
+++ FitGenius/src/services/api.ts
@@ -1,6 +1,6 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const API_URL = 'https://fitgenius.rincon-z.com/api';
+const API_URL = 'https://api.fitgenius.rincon-z.com/api';
 
 class ApiService {
   private token: string | null = null;
